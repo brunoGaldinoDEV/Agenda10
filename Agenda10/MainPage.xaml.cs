1be531dd4a6afcb3766f811dd13b0cffadcb6d73
Brunogaldinoarruda@Gmail.com <Brunogaldinoarruda@Gmail.com>
2025-06-01 12:13:40 -0300

    Adicionar arquivos de projeto.

---
+++ Agenda10/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace Agenda10
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+                string msg = "";
+                if(etanol <= (gasolina * 0.7))
+                {
+                    msg = " o etanol está compensando.";
+                }
+                else
+                {
+                    msg = "A gasolina está compensando";
+                }
+
+                DisplayAlert("Calculado ", msg, "Ok");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops:", ex.Message, "Fechar");
+            }
+
+
+        }
+    }
+
+}
